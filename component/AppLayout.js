80d4eb2ee84e39fb5287d0f7f5cfdc9a512933e1
sbyiadom <sbyiadom88@gmail.com>
2026-01-25 21:28:43 +0000

    Create AppLayout.js

---
+++ component/AppLayout.js
@@ -0,0 +1,19 @@
+export default function AppLayout({ children, background }) {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        backgroundImage: `linear-gradient(
+          rgba(0,0,0,0.55),
+          rgba(0,0,0,0.55)
+        ), url(${background})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        padding: '40px'
+      }}
+    >
+      {children}
+    </div>
+  );
+}
